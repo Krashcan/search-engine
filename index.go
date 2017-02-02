cf9da8c0fe8442a93af3a2403b699094fed64cdf
Kumar Ashish <kr.ashish8196@gmail.com>
2017-02-03 03:07:58 +0530

    finished data importing using index.go

--- index.go
+++ index.go
@@ -1,12 +1,11 @@
 package main
 
 import(
-	//"gopkg.in/olivere/elastic.v5"
-	"fmt"
+	"gopkg.in/olivere/elastic.v5"
+	"golang.org/x/net/context"
 	"log"
-	//"net/http"
 	"os"
-	//"strings"
+	//"fmt"
 	"encoding/json"
 )
 
@@ -21,37 +20,48 @@ type item struct{
 	Id string `json:"id"`
 	Title string `json:"title"`
 	Journal  string `json:"journal"`
-	Volume string `json:"volume"`
-	Number string `json:"number"`
+	Volume int `json:"volume"`
+	Number int `json:"number"`
 	Pages string `json:"pages"`
-	Year string `json:"year"`
+	Year int `json:"year"`
 	Authors []people `json:"authors"`
 	Abstract string `json:"abstract"`
 	Link string `json:"link"`
 	Keywords []string `json:"keywords"`
 	Body string `json:"body"`
 }
-//For decoding our json file, we will use this array. Notice the type item and people.
-var data []item
 
-/*func init(){
-	client,err := elastic.NewClient()
+var client *elastic.Client
+var err error
+func init(){
+	client,err = elastic.NewClient()
 	if err!=nil{
 		log.Fatal(err)
 	}
-}*/
+}
 
 func main() {
+	var data []item
+	
 	file,err := os.Open("data.json")
 	if err!=nil{
 		log.Fatal(err)
-	}	
+	}
 	defer file.Close()
+	
 	jsonDeocder := 	json.NewDecoder(file)
 	if err := jsonDeocder.Decode(&data); err!=nil{
 		log.Fatal("Decode: ",err)
 	}
-	fmt.Println(data[0].Id)
+	bulkIndex("library","article",data)
 }
 
-//func bulkIndex(index string,type string,)
+func bulkIndex(index string,typ string ,data []item){
+	ctx := context.Background()
+	for _,item := range data{
+		_,err := client.Index().Index(index).Type(typ).Id(item.Id).BodyJson(item).Do(ctx)	
+		if err !=nil{
+			log.Fatal(err)
+		}
+	}
+}
